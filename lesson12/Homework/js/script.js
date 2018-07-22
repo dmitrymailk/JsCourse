a528509a77eadb83d692aacc1069c48ddf9615ca
dimweb <dmitrymail_k@mail.ru>
2018-07-22 10:44:09 +0300

    lesson12 21.07.18

---
+++ lesson12/Homework/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.sheldure').on('click', function(){
+		$('.modal').fadeIn();
+	});
+	$('.close').on('click', function(){
+		$('.modal').fadeOut();
+	});
+
+	$('.main_btna').on('click', function(){
+		$('.modal').fadeIn();
+	});
+	$('.close').on('click', function(){
+		$('.modal').fadeOut();
+	});
+
+	$('.main_btn').on('click', function(){
+		$('.modal').fadeIn();
+	});
+	$('.close').on('click', function(){
+		$('.modal').fadeOut();
+	});
+	
+});
