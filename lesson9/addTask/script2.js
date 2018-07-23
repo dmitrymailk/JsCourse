e278498844617bd18f2809ea1d989be8f10af750
dimweb <dmitrymail_k@mail.ru>
2018-07-23 10:38:59 +0300

    lesson9 20.07.18

---
+++ lesson9/addTask/script2.js
@@ -0,0 +1,9 @@
+
+let age = document.getElementById('age');
+
+
+function showUser(surname, name,age) {
+ console.log("Пользователь " + surname + " " + name + ", его возраст " +
+age.value);
+}
+showUser('qwe','ewqq',age);
